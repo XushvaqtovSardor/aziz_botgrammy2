4610670c1680cb7b6490a2f45eae0a3791661198
fn25 <xushvaqtovsardor032@gmail.com>
2026-01-28 18:53:21 +0500

    fasdf dsf 4

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -20,7 +20,7 @@ export class PrismaService
     const databaseUrl = process.env.DATABASE_URL;
 
     if (!databaseUrl) {
-      throw new Error('❌ DATABASE_URL environment variable is not set');
+      throw new Error('❌ DATABASE_URL  environment variable is not set');
     }
 
     const maskedUrl = databaseUrl.replace(/:[^:@]+@/, ':****@');
